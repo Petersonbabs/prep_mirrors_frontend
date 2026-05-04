710a3e7531c276ae4122974942c196050bdd7e65
petersonbabs <timiebabs@gmail.com>
2026-05-04 12:56:50 +0100

    Added redirect to waitlist

--- src/components/WaitListRedirect.tsx
+++ src/components/WaitListRedirect.tsx
@@ -9,10 +9,12 @@ const APP_READY = import.meta.env.VITE_APP_READY === 'true';
 export function WaitlistRedirect({ children }: WaitlistRedirectProps) {
   const isWaitlistPage = window.location.pathname === 'https://waitlist.prepmirrors.com';
   const isProd = import.meta.env.VITE_ENVIRONMENT === "production"
+  const isAdmin = localStorage.getItem("testKey") ?? null
 
   // If app is not ready and not already on waitlist page, redirect
-  if (!APP_READY && !isWaitlistPage && isProd) {
-    return window.location.href = "https://waitlist.prepmirrors.com"
+  if (!APP_READY && !isWaitlistPage && isProd && isAdmin === '1234$#@!') {
+    window.location.href = "https://waitlist.prepmirrors.com"
+    return null 
   }
 
   return <>{children}</>;
